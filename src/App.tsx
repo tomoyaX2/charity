812d52ab199c53c5175b82676f021e686673a1bd
tomoya <kalinichenko1999@gmail.com>
2023-02-25 14:38:09 +0200

    add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-import { FacebookIcon } from "./components/icons/facebook";
 import image1 from "./img/1.jpg";
 import image2 from "./img/2.jpg";
 import image3 from "./img/3.jpg";
 import avatar1 from "./img/avatar1.jpg";
 import avatar2 from "./img/avatar2.jpg";
 import avatar3 from "./img/avatar3.jpg";
-import phone from "./img/phone.png";
-import email from "./img/email.jpg";
+import "rsuite/dist/rsuite.min.css";
+import { Header } from "./components/Header";
 
 const carouselLinks = [image1, image2, image3];
 
@@ -24,17 +23,8 @@ function App() {
   }, [activeLinkIndex]);
 
   return (
-    <div className="flex flex-col w-full pb-20">
-      <div className="h-40 w-full border-b-2 border-gray-200 flex items-center justify-center">
-        <img src={image3} alt="" className="w-64 h-32" />
-      </div>
-      <div className="w-full flex flex-row items-center justify-center border-b-2 border-gray-200">
-        <span className="px-8 py-2 cursor-pointer">Main</span>
-        <span className="px-8 py-2 cursor-pointer">Programs</span>
-        <span className="px-8 py-2 cursor-pointer">Partners</span>
-        <span className="px-8 py-2 cursor-pointer">Founders</span>
-        <span className="px-8 py-2 cursor-pointer">Contact Us</span>
-      </div>
+    <>
+      <Header />
       <div className="lg:px-40 sm:px-20 px-10 mt-8">
         <div className=" border-b-2 border-gray-200">
           Lorem Ipsum is simply dummy text of the printing and typesetting
@@ -136,35 +126,9 @@ function App() {
               only
             </span>
           </div>
-          <div className="w-full flex items-center justify-center border-t-2 border-gray-200 mt-12">
-            <div className="flex flex-col items-center justify-center w-full">
-              <h2 className="mt-4 w-full text-center font-semibold uppercase text-3xl  border-b-2 border-gray-200 pb-4">
-                Contact Us:
-              </h2>
-              <div className="flex flex-col mt-4 w-48">
-                <div className="flex flex-row">
-                  <img src={phone} alt="" className="w-4 h-4 mr-4 mt-1" />
-                  <div className="flex flex-col">
-                    <span>0969500909</span>
-                    <span>0504490949</span>
-                    <span>0663474444</span>
-                  </div>
-                </div>
-                <div className="flex flex-row mt-2">
-                  <img src={email} alt="" className="w-5 h-5 mr-3 mt-1" />
-                  <div className="flex flex-col">
-                    <span>ukrnativ22@gmail.com</span>
-                  </div>
-                </div>
-                <div className="flex items-center justify-center">
-                  <FacebookIcon className="mt-4 cursor-pointer" />
-                </div>
-              </div>
-            </div>
-          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,163 @@
+import { Drawer } from "rsuite";
+import { FacebookIcon } from "../icons/facebook";
+import phone from "../../img/phone.png";
+import email from "../../img/email.jpg";
+import logo from "../../img/logo.jpg";
+import MenuIcon from "@rsuite/icons/Menu";
+import { useState } from "react";
+
+const Header = () => {
+  const [isDrawerOpenned, setDrawerOpenned] = useState(false);
+
+  return (
+    <>
+      <Drawer
+        open={isDrawerOpenned}
+        className="!w-64"
+        onClose={() => {
+          setDrawerOpenned(false);
+        }}
+        size="md"
+      >
+        <Drawer.Header>
+          <Drawer.Title>Contact Us</Drawer.Title>
+        </Drawer.Header>
+        <Drawer.Body className="!px-4">
+          <>
+            <div className="flex flex-col">
+              <div className="flex mt-2 mr-2">
+                <img src={phone} alt="" className="w-4 h-4 mr-2" />
+                <a
+                  href="tel:+380969500909"
+                  className="pointer text-black hover:text-black"
+                >
+                  +380969500909
+                </a>
+              </div>
+              <div className="flex mt-2 mr-2">
+                <img src={phone} alt="" className="w-4 h-4 mr-2" />
+                <a
+                  href="tel:+380504490949"
+                  className="pointer text-black hover:text-black"
+                >
+                  +380504490949
+                </a>
+              </div>
+
+              <div className="flex mt-2 mr-2">
+                <img src={phone} alt="" className="w-4 h-4 mr-2" />
+                <a
+                  href="tel:+38 0663474444"
+                  className="pointer text-black hover:text-black"
+                >
+                  +380663474444
+                </a>
+              </div>
+              <div className="flex flex-row mt-2">
+                <img src={email} alt="" className="w-5 h-5 mr-2" />
+                <div className="flex flex-col">
+                  <a
+                    href="mailto:ukrnativ22@gmail.com"
+                    className="pointer text-black hover:text-black"
+                  >
+                    ukrnativ22@gmail.com
+                  </a>
+                </div>
+              </div>
+            </div>
+            <a
+              className="flex items-center justify-start text-black"
+              target="_blank"
+              rel="noreferrer"
+              href="https://www.facebook.com/profile.php?id=100081320017600"
+            >
+              <FacebookIcon className="mt-4 cursor-pointer" />
+              <span className="mt-4 ml-2">Україно Рідна</span>
+            </a>
+          </>
+        </Drawer.Body>
+      </Drawer>
+      <div className="flex flex-col w-full pb-20">
+        <div className="w-full flex flex-row items-center justify-between border-b-2 border-gray-200 xsm:px-20 lg:px-40 py-4">
+          <div className="flex items-center">
+            <img
+              src={logo}
+              alt=""
+              className="lg:w-32 lg:h-32 sm:w-24 sm:h-24 xsm:w-16 xsm:h-16 rounded-sm"
+            />
+            <h3 className="italic ml-4">Україно Рідна</h3>
+          </div>
+          <div
+            className="xsm:block md:hidden cursor-pointer"
+            onClick={() => {
+              setDrawerOpenned(true);
+            }}
+          >
+            <MenuIcon />
+          </div>
+
+          <div className="flex xsm:hidden md:block">
+            <div className="flex flex-row items-start justify-center items-center">
+              <span className="font-semibold text-lg mr-4 h-full ">
+                Contact Us:{" "}
+              </span>
+
+              <div className="flex flex-col">
+                <div className="flex mt-2 mr-2">
+                  <img src={phone} alt="" className="w-4 h-4 mr-2" />
+                  <a
+                    href="tel:+380969500909"
+                    className="pointer text-black hover:text-black"
+                  >
+                    +380969500909
+                  </a>
+                </div>
+                <div className="flex mt-2 mr-2">
+                  <img src={phone} alt="" className="w-4 h-4 mr-2" />
+                  <a
+                    href="tel:+380504490949"
+                    className="pointer text-black hover:text-black"
+                  >
+                    +380504490949
+                  </a>
+                </div>
+
+                <div className="flex mt-2 mr-2">
+                  <img src={phone} alt="" className="w-4 h-4 mr-2" />
+                  <a
+                    href="tel:+38 0663474444"
+                    className="pointer text-black hover:text-black"
+                  >
+                    +380663474444
+                  </a>
+                </div>
+                <div className="flex flex-row mt-2">
+                  <img src={email} alt="" className="w-5 h-5 mr-2" />
+                  <div className="flex flex-col">
+                    <a
+                      href="mailto:ukrnativ22@gmail.com"
+                      className="pointer text-black hover:text-black"
+                    >
+                      ukrnativ22@gmail.com
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <a
+                className="flex items-center justify-center"
+                target="_blank"
+                rel="noreferrer"
+                href="https://www.facebook.com/profile.php?id=100081320017600"
+              >
+                <FacebookIcon className="mt-4 cursor-pointer" />
+                <span className="mt-4 ml-2">Україно Рідна</span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export { Header };
